488ade5eabd1d2261ac9f1f9a8753b61b02b9b9a
Singh <eltigre.svo@gmail.com>
2020-01-29 15:48:00 +0100

    Added Filewriter class

---
+++ src/nl/jparengkuan/FileWriter.java
@@ -0,0 +1,61 @@
+package nl.jparengkuan;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class FileWriter {
+    static HashMap<String,FileOutputStream> open_streams;
+    public static FileOutputStream  OpenFile(File file,String path){
+        FileOutputStream writer=null;
+        try {
+
+
+
+            if (!file.exists()) {
+                file.createNewFile();
+
+
+
+            }
+            writer = new FileOutputStream(path, true);
+            open_streams.put(path,writer);
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+        return writer;
+
+    }
+
+
+
+
+    public static void writeData(LinkedList<String> weatherData) {
+        try {
+            Path currentDir = FileSystems.getDefault().getPath("");
+
+            String path = currentDir + (String) weatherData.get(2) + ".bin";
+
+            File file = new File(path);
+            FileOutputStream writer=OpenFile(file,path);
+
+
+            writer.write((weatherData.get(2)+","+weatherData.get(4)+","+weatherData.get(6)
+                    +","+weatherData.get(8)+","+weatherData.get(10)+","+weatherData.get(12)
+                    +","+weatherData.get(14)+","+weatherData.get(16)+","+weatherData.get(18)
+                    +","+weatherData.get(20)+","+weatherData.get(22)+","+weatherData.get(24)+","+weatherData.get(28)).getBytes() );
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
