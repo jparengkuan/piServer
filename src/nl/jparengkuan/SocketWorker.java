d5b9673938ee60c854a6a55c2751fcc76ec7c6bd
Singh <eltigre.svo@gmail.com>
2020-01-29 20:03:52 +0100

    ACtivated FileWriter

--- src/nl/jparengkuan/SocketWorker.java
+++ src/nl/jparengkuan/SocketWorker.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class SocketWorker implements Runnable {
@@ -30,12 +32,24 @@ public class SocketWorker implements Runnable {
             String buffer = "";
 
             while ((input = in.readLine()) != null){
+                if (input.equals("\t<MEASUREMENT>")) {
+                    buffer = "";
+                }
+
+                buffer = buffer + input;
+
+                if (input.equals("\t</MEASUREMENT>")) {
+
+                 LinkedList<String> data=DataParser.DataParser(buffer);
 
 
 
+                    FileWriter.writeData(data);
 
 
-            }
+                }
+
+                }
 
             //in.close(); // Close the stream
 
