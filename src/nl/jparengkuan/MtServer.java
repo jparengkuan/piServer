91ec17cadc675fa3813199e4daa47eb4eafa50e2
Jim <jim.parengkuan@gmail.com>
2020-01-31 20:29:16 +0100

    Code cleaning, custom error en delay verhoogt

--- src/nl/jparengkuan/MtServer.java
+++ src/nl/jparengkuan/MtServer.java
@@ -11,62 +11,39 @@ public class MtServer implements Runnable {
 
     protected int serverPort;
     protected ServerSocket serverSocket = null;
-    protected boolean isStopped = false;
-    protected Thread runningThread = null;
     protected ScheduledExecutorService executor;
 
     protected int delay = 0;
 
 
     public MtServer(int port) {
-
         this.serverPort = port;
-
-        this.executor = Executors.newScheduledThreadPool(1000);
+        this.executor = Executors.newScheduledThreadPool(800);
     }
 
     @Override
     public void run() {
-        // synchronize the action of multiple threads and make sure that only one thread
-        // can access the resource at a given point in time
 
-
+        //Open een nieuwe serverSocket
         openServerSocket();
 
-        while (!isStopped()) {
-            Socket clientSocket = null;
-            try {
-
-                clientSocket = this.serverSocket.accept();
-
-            } catch (IOException e) {
-                if (isStopped()) {
-                    System.out.println("Server stopped!");
-                    return;
-                }
-
-                throw new RuntimeException("Error accepting client connection!", e);
-            }
+        Socket clientSocket = null;
 
 
-
-            executor.schedule(new SocketWorker(clientSocket), this.delay , TimeUnit.MILLISECONDS);
-
-            this.delay += 150;
-
-
-            //SocketWorker worker = new SocketWorker(clientSocket);
-            //new Thread(worker).start();
-
-
-
+        // Try accepting the incoming connection
+        // Else throw a runtime error
+        try {
+            clientSocket = this.serverSocket.accept();
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot open accept the connection: ", e);
         }
 
+        //Create a new scheduled task
+        executor.schedule(new SocketWorker(clientSocket), this.delay, TimeUnit.MILLISECONDS);
 
-    }
+        //Increase the delay
+        this.delay += 200;
 
-    private synchronized boolean isStopped() {
-        return this.isStopped;
     }
 
 
